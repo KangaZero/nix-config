9e7de118aa27a8170c98d385e97eafb831924aac
KangaZero <samuelyongw@gmail.com>
2026-06-19 00:55:27 +0900

    feat(hooks): add pre-push check to reject non-KangaZero author commits

--- lib/mkChecks.nix
+++ lib/mkChecks.nix
@@ -9,6 +9,30 @@ inputs.git-hooks.lib.${system}.run {
     nixfmt.enable = true;
     statix.enable = true;
     deadnix.enable = true;
+    check-author = {
+      enable = true;
+      name = "check git author";
+      entry = "${pkgs.writeShellScript "check-author" ''
+        while IFS=" " read -r _local_ref local_sha _remote_ref remote_sha; do
+          [ "$local_sha" = "0000000000000000000000000000000000000000" ] && continue
+          if [ "$remote_sha" = "0000000000000000000000000000000000000000" ]; then
+            range="$local_sha"
+          else
+            range="$remote_sha..$local_sha"
+          fi
+          bad=$(git log --format="%H %ae" "$range" | grep -v "samuelyongw@gmail.com")
+          if [ -n "$bad" ]; then
+            echo "Push rejected: commits not authored by KangaZero <samuelyongw@gmail.com>:"
+            echo "$bad"
+            exit 1
+          fi
+        done
+      ''}";
+      language = "system";
+      pass_filenames = false;
+      always_run = true;
+      stages = [ "pre-push" ];
+    };
     home-build = {
       enable = true;
       name = "system build";
